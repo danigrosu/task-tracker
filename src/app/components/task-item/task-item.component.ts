29c0e67657d470956a573c5b3b27a9eb12490900
unknown <dni.grosu@gmail.com>
2021-09-09 21:21:51 +0300

    Delete method added

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Tasks'
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task | undefined;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+
+  faTimes = faTimes;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onDelete(task: Task | undefined) {
+    this.onDeleteTask.emit(task);
+  }
+
+}
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import {Task} from './Tasks'
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'may 5th at 2:30pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Meeting at school',
+        day: 'may 6th at 3:30pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Shopping',
+        day: 'may 7th at 5:30pm',
+        reminder: false
+    },
+    {
+        id: 4,
+        text: 'New haircut',
+        day: 'may 9th at 1:30pm',
+        reminder: true
+    },
+]
